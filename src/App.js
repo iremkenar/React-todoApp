c94a0211c0fc58e175a02bcf2fd5cf5a9fe7e6d2
iremkenar <irem.kenar@outlook.com>
2021-05-20 17:50:35 +0300

    Added from and todolist components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+//Importing components
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState('');
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <header>
+        <h1>Irem's ToDo List</h1>
+      </header>
+      <Form onSetInputText={setInputText} />
+      <TodoList />
     </div>
   );
 }
